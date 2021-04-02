663bbdc876bbb4de51c907dfccb0338c7f977912
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-04-02 14:15:46 -0400

    Update mock-students.ts

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -1,9 +1,9 @@
 import { Students } from './students';
 
 export const STUDENTS: Students[] = [
-  { ranking: 1, name: 'Marquis', age: 31, feedback: "Gets confused for Brice" },
-  { ranking: 2, name: 'Ibtxhis', age: 28, feedback: "Talks about everyone (Gossip Girl)" },
-  { ranking: 3, name: 'Wiynter', age: 29, feedback: "Really chill but can get mad easily" },
-  { ranking: 4, name: 'Marcus', age: 23, feedback: "Intelligent but asks way too many long-winded questions" },
-  { ranking: 5, name: 'Eyke', age: 27, feedback: "Outgoing but speaks over everyone..." }
+  { ranking: 1, name: 'Marquis', age: 31, feedback: "Gets confused for Brice", image: '' },
+  { ranking: 2, name: 'Ibtxhis', age: 28, feedback: "Talks about everyone (Gossip Girl)", image: '' },
+  { ranking: 3, name: 'Wiynter', age: 29, feedback: "Really chill but can get mad easily", image: '' },
+  { ranking: 4, name: 'Marcus', age: 23, feedback: "Intelligent but asks way too many long-winded questions", image: "https://media-exp1.licdn.com/dms/image/C4E03AQEYEbd8xae8JA/profile-displayphoto-shrink_100_100/0/1576643932617?e=1622678400&v=beta&t=5zrAEQHR65KFTrpjrSSy5f60kWgDSNEaVOP90DrIF8s" },
+  { ranking: 5, name: 'Eyke', age: 25, feedback: "Outgoing but speaks over everyone...", image: 'https://us04images.zoom.us/p/s0XpYfEaSuK2GEDpFKKMXw/f12b0ec7-20c7-4acd-ae21-a81ab08f88cc-1273' }
 ];
